b424122586c7b96c64e7ec771d187c9dea848f34
Jorrit Poelen <1084872+jhpoelen@users.noreply.github.com>
2022-10-18 15:43:05 -0500

    related to https://github.com/bio-guoda/preston-brit-2022/issues/3 ; fyi @jbest

--- list-image-urls.sh
+++ list-image-urls.sh
@@ -6,6 +6,5 @@
 preston ls\
  | preston dwc-stream\
  | grep URI\
- | jq --raw-output '.["http://rs.tdwg.org/ac/terms/accessURI" | "http://rs.tdwg.org/ac/terms/thumbnailAccessURI" | "http://rs.tdwg.org/ac/terms/goodQualityAccessURI"]'
-
-
+ | jq --raw-output '.["http://rs.tdwg.org/ac/terms/accessURI"], .["http://rs.tdwg.org/ac/terms/thumbnailAccessURI"], .["http://rs.tdwg.org/ac/terms/goodQualityAccessURI"]'\
+ | grep -v null
