8611128dc74f8dcc7d0272b363083473b386c5ad
Jorrit Poelen <1084872+jhpoelen@users.noreply.github.com>
2023-02-13 15:21:28 -0600

    Update list-bisque-cyverse-original-images.sh

--- list-bisque-cyverse-original-images.sh
+++ list-bisque-cyverse-original-images.sh
@@ -8,6 +8,7 @@ preston ls\
 | grep URI\
 | jq --raw-output '.["http://rs.tdwg.org/ac/terms/accessURI"], .["http://rs.tdwg.org/ac/terms/thumbnailAccessURI"], .["http://rs.tdwg.org/ac/terms/goodQualityAccessURI"]'\
 | grep -v null\
+| grep bisque\
 | grep cyverse\
 | grep -oP "00-[a-zA-Z0-9]+"\
 | sed 's+^+https://bisque.cyverse.org/blob_service/+g'
